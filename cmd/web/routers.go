4a7cfb9607572c3cd2bd634a969a3ac5d795b867
brainwhat <facelessbeach@gmail.com>
2025-01-15 02:19:50 +0300

    added julienschmidt/httprouter

--- cmd/web/routers.go
+++ cmd/web/routers.go
@@ -3,27 +3,48 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
-// That's a stdlib implementation
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	router := httprouter.New()
 
-	// Here we bind our FileServer to the /static folder
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// So here let's say we request for /static/css/main.css
-	// There is no /static folder inside /ui/static
-	// So we need to strip this part
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
-	// Alice is a simple middleware chainer
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	return standard.Then(mux)
-
+	return standard.Then(router)
 }
+
+// That's a stdlib implementation
+// func (app *application) routes() http.Handler {
+// 	mux := http.NewServeMux()
+
+// 	// Here we bind our FileServer to the /static folder
+// 	fileServer := http.FileServer(http.Dir("./ui/static/"))
+// 	// So here let's say we request for /static/css/main.css
+// 	// There is no /static folder inside /ui/static
+// 	// So we need to strip this part
+// 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+// 	mux.HandleFunc("/", app.home)
+// 	mux.HandleFunc("/snippet/view", app.snippetView)
+// 	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+// 	// Alice is a simple middleware chainer
+// 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+// 	return standard.Then(mux)
+
+// }
